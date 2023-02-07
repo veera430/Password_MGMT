2c4fd537461c217ebb109a547d3b84c25c303913
veera430 <veerababugollavilli@gmail.com>
2023-02-07 21:37:15 +0530

    modified set and docker file

--- emailproject/settings.py
+++ emailproject/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-(*%xo3m9b7&!e4u^*!j*gd&7mgc)9i()g=p*&-x0fce2rnu3s-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
